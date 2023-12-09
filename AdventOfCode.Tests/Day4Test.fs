a3958ea74d1230d83d97586f3a4cfb51cdcafcb3
Georgii Korshunov <kipelovets@mail.ru>
2023-12-10 00:10:11 +0100

    ✨ Day4.1, using FsUnit

---
+++ AdventOfCode.Tests/Day4Test.fs
@@ -0,0 +1,43 @@
+module AdventOfCode.TestsDay4
+
+open NUnit.Framework
+open AdventOfCode
+open System.IO
+open FsUnit
+
+[<TestFixture>]
+type Day4Test () =
+
+    let realInput = "day4.input" 
+                    |> File.ReadAllText 
+                    |> Common.SplitInput
+                    |> Array.map Day4.parseInputLine
+
+    let expectedFirstSampleLine = ([| 41; 48; 83; 86; 17 |], [| 83; 86;  6; 31; 17;  9; 48; 53 |])
+
+    [<Test>]
+    member _.parseInputLine() =
+        Day4.parseInputLine "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+            |> should equal expectedFirstSampleLine
+
+    [<Test>]
+    member _.sampleInput() =
+        Day4.sampleInput[0] |> should equal expectedFirstSampleLine
+
+    [<TestCase(0, 8)>]
+    [<TestCase(1, 2)>]
+    [<TestCase(2, 2)>]
+    [<TestCase(3, 1)>]
+    [<TestCase(4, 0)>]
+    [<TestCase(5, 0)>]
+    member _.cardValue(cardInd: int, expected: int) =
+        Day4.cardValue Day4.sampleInput[cardInd] |> should equal expected
+
+    [<Test>]
+    member _.cardPileValueSample() =
+        Day4.cardPileValue Day4.sampleInput |> should equal 13
+
+
+    [<Test>]
+    member _.cardPileValueReal() =
+        Day4.cardPileValue realInput |> should equal 15268
