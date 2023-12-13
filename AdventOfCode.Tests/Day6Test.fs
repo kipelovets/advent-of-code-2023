e0e0aad023dd025166cc2d514671eece1635e853
Georgii Korshunov <kipelovets@mail.ru>
2023-12-13 23:55:04 +0100

    ✨ Day6.2

--- AdventOfCode.Tests/Day6Test.fs
+++ AdventOfCode.Tests/Day6Test.fs
@@ -15,11 +15,11 @@ Distance:  9  40  200" |> Common.SplitInput |> Array.toList
     [<Test>]
     member _.parseInput() =
         let result = Day6.parseInput sampleInput
-        result |> should equal [(7, 9); (15, 40); (30, 200)]
+        result |> should equal [(7L, 9L); (15L, 40L); (30L, 200L)]
 
     [<Test>]
     member _.winOptions() =
-        Day6.winOptions (7, 9) |> should equal [2; 3; 4; 5]
+        Day6.winOptions (7, 9) |> should equal [2L; 3L; 4L; 5L]
         Day6.winOptions (15, 40) |> should haveLength 8
         Day6.winOptions (30, 200) |> should haveLength 9
         
@@ -27,3 +27,12 @@ Distance:  9  40  200" |> Common.SplitInput |> Array.toList
     member _.errorMargin() =
         Day6.errorMargin (Day6.parseInput sampleInput) |> should equal 288
         Day6.errorMargin (Day6.parseInput realInput) |> should equal 4568778
+
+    [<Test>]
+    member _.parseInput2() =
+        Day6.parseInput2 sampleInput |> should equal (71530L, 940200L)
+        Day6.parseInput2 realInput |> should equal (48989083L, 390110311121360L)
+
+    [<Test>]
+    member _.part2() =
+        Day6.errorMargin [Day6.parseInput2 realInput] |> should equal 28973936
--- AdventOfCode/Day6.fs
+++ AdventOfCode/Day6.fs
@@ -1,7 +1,7 @@
 namespace AdventOfCode
 
 module Day6 =
-    type Race = int*int
+    type Race = int64*int64
 
     let After (search: string) (str: string) = 
         str[str.IndexOf(search) + 1..]
@@ -9,20 +9,20 @@ module Day6 =
     let Split (by: string) (str: string) =
         str.Split(by) 
 
-    let parseInputLine (line: string): int array =
+    let parseInputLine (line: string): int64 array =
         line 
             |> (After ":") 
             |> (Split " ") 
             |> Array.filter (fun s -> s.Length > 0)
-            |> Array.map int
+            |> Array.map int64
 
     let parseInput (input: string list): Race list = 
         let [times; distances] = input |> List.map parseInputLine
         [ for i in [0..times.Length - 1] do (times[i], distances[i]) ]
 
-    let winOptions ((time, dist): Race): int list =
-        let min = 1
-        let max = time - 1
+    let winOptions ((time, dist): Race): int64 list =
+        let min = 1L
+        let max = time - 1L
         [for i in min..max do 
             if (time - i) * i > dist then i]
 
@@ -30,3 +30,9 @@ module Day6 =
         races 
             |> List.map (fun race -> (winOptions race).Length) 
             |> List.fold (*) 1
+
+    let parseInput2 (input: string list): Race =
+        let [time; dist] = input 
+                            |> List.map (fun str -> (After ":" str).Replace(" ", "") |> int64 )
+        (time, dist)
+            
